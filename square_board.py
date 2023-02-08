c88db81c81d2097320592d6eceb4badb117c66db
Fernando Cuellar <fercuellar@gmail.com>
2023-02-07 18:05:01 -0700

    comenzando con la parte del orecle_test

--- square_board.py
+++ square_board.py
@@ -1,4 +1,5 @@
 from linear_board import LinearBoard
+from list_utils import transpose, displace_matrix, reverse_matrix
 from settings import BOARD_LENGTH
 
 
@@ -23,6 +24,11 @@ class SquareBoard():
             result = result and lb.is_full()
         return result
     
+    def as_matrix(self):
+        #devuelve una represenatcion en formato matiz es decir lista de listas
+        return list(map(lambda x: x._column, self._columns))
+
+    
     # Detetctar victoria
     def is_victory(self, char):
         return self._any_vertical_victory(char) or self._any_horizontal_victory(char) or self._any_rising_victory(char) or self._any_sinking_victory(char)
@@ -33,15 +39,37 @@ class SquareBoard():
             result = result or lb.is_victory(char)
         return result
     
-    def _any_horizontal_victory(self, cahr):
-        return False
+    def _any_horizontal_victory(self, char):
+        # Transponemos _columns
+        transp = transpose(self.as_matrix())
+        # Creamos un tablero temporal con esa matriz transpuesta
+        tmp = SquareBoard.fromList(transp)
+
+        # comprobamos si tiene una victoria temporal
+        return tmp._any_vertical_victory(char)
+        
     
     def _any_rising_victory(self, char):
-        return False
+        # Obtener las columnas
+        m = self.as_matrix
+        # Las invertimos
+        rm = reverse_matrix(m)
+        # Creamos tablero temporal con esa matriz
+        tmp = SquareBoard.fromList(rm)
+        # Devolvemos si tiene una victoria descendente
+        return tmp._any_sinking_victory(char)
     
     def _any_sinking_victory(self, char):
-        return False
+        # Obtenemos las columnas como una mariz
+        m = self.as_matrix()
+        # Las despalzamos al final
+        d = displace_matrix(m)
+        # Creamos un tablero temporal con esa matriz
+        tmp = SquareBoard.fromList(d)
+        # Averiguamos si tiene una victoria horizontal
+        return tmp._any_horizontal_victory(char)
     
+
     # Dunders metodos magicos para hacer clases
 
     def __repr__(self):
--- square_board_test.py
+++ square_board_test.py
@@ -2,44 +2,54 @@ import pytest
 
 from square_board import *
 
+
 def test_empty_board():
+
     board = SquareBoard()
 
     assert board.is_full() == False
     assert board.is_victory('O') == False
     assert board.is_victory('X') == False
 
+
 def test_vertical_victory():
-    vertical = SquareBoard.fromList([['O', 'X', 'X', 'X',
+    vertical = SquareBoard.fromList([['O', 'X', 'X', 'X', ],
                                     [None, None, None, None, ],
                                     [None, None, None, None, ],
                                     [None, None, None, None, ],
-                                    [None, None, None, None, ]]])
+                                    [None, None, None, None, ]])
+    
     assert vertical.is_victory('X')
     assert vertical.is_victory('O') == False
 
+
 def test_horizontal_victory():
-    horizontal_victory = SquareBoard.fromList([['X', None, None, None, None, ],
-                                                ['X', None, None, None, None, ],
-                                                ['X', 'O', None, None, None, ],
-                                                ['X', 'O', None, None, None, ],
-                                                ['X', 'O', None, None, None, ]])
-    assert horizontal_victory.is_victory('X')
+    horizontal_victory = SquareBoard.fromList([['x', None, None, None, None, ],
+                                                ['x', None, None, None, None, ],
+                                                ['x', 'o', None, None, None, ],
+                                                ['x', 'o', None, None, None, ],
+                                                ['x', 'o', None, None, None, ]])
+
+    assert horizontal_victory.is_victory('x')
+
 
 def test_sinking_victory():
-    sinking_victory = SquareBoard.fromList([['X', 'O', 'X', 'O', ],
-                                            ['X' , 'X', 'O', None, ],
-                                            ['O', 'O', None, None, ],
-                                            ['O', 'X', None, None, ],
-                                            ['X', None, None, None, ]])
-    assert sinking_victory.is_victory('O')
-    assert sinking_victory.is_victory('X') == False
+    sinking_victory = SquareBoard.fromList([['x', 'o', 'x', 'o', ],
+                                            ['x', 'x', 'o', None, ],
+                                            ['o', 'o', None, None, ],
+                                            ['o', 'x', None, None, ],
+                                            ['x', None, None, None, ]])
+    
+    assert sinking_victory.is_victory('o')
+    assert sinking_victory.is_victory('x') == False
+
 
 def test_rising_victory():
-    rising_victory = SquareBoard.fromList([['X', 'O', None, None, ],
-                                            ['O', 'X', None, None, ],
-                                            ['X', 'O', 'X', 'O', ],
-                                            ['X', 'O', 'O', 'X', ],
-                                            ['O', 'X', 'O', None, ]])
-    assert rising_victory.is_victory('X')
-    assert rising_victory.is_victory('O') == False
+
+    rising_victory = SquareBoard.fromList([['x', 'o', None, None, ],
+                                            ['o', 'x', None, None, ],
+                                            ['x', 'o', 'x', 'o', ],
+                                            ['x', 'o', None, None, ], ])
+    
+    assert rising_victory.is_victory('x')
+    assert rising_victory.is_victory('o') == False
